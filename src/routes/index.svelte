90bba4ec5fcea82fe52781a957e6ec6f3af11315
oe.kazuma <oe.kazuma@a-tm.co.jp>
2021-05-22 00:24:47 +0900

    update deps

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,87 @@
-<script lang="ts">
-  let toggle: boolean = true;
-  let classes: string = 'text-red-500 text-sm';
-</script>
-
-<button on:click={() => (toggle = !toggle)}>Toggle [{toggle}]</button>
-
-<p class={toggle ? 'bg-red-800 text-4xl' : 'bg-blue-800 text-6xl'}>toggleで動く</p>
-
-<p class="text-blue-500">動く</p>
-<p class="text-blue-400">動く</p>
-
-<p class={'text-blue-300'}>動かない</p>
-<p class={'text-blue-200'}>動かない</p>
-<p class={`text-blue-100`}>動かない</p>
-
-<p class={classes}>動かない</p>
-
-<div class={toggle ? 'bg-blue-500' : 'bg-black'}>
-  <p class="text-lg {toggle ? 'text-green-500' : 'text-white'}">動く</p>
+<!-- code snippet from https://play.tailwindcss.com/ by Tailwind Labs -->
+<div class="flex flex-col min-h-screen bg-gray-100 py-6 justify-center sm:py-12">
+  <div class="py-4 relative sm:(mx-auto max-w-xl)">
+    <div
+      class="bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform inset-0 -skew-y-6 absolute sm:(rounded-3xl -rotate-6 skew-y-0)"
+    />
+    <div class="bg-white shadow-lg py-10 px-4 relative sm:(rounded-3xl p-20)">
+      <div class="mx-auto max-w-md">
+        <div class="divide-y divide-gray-200">
+          <div class="space-y-4 text-base py-8 text-gray-700 leading-6 sm:(text-lg leading-7)">
+            <p>
+              An advanced online playground for Tailwind CSS, including support for things like:
+            </p>
+            <ul class="list-disc space-y-2">
+              <li class="flex items-start">
+                <span class="flex h-6 items-center sm:h-7">
+                  <svg
+                    class="flex-shrink-0 h-5 text-cyan-500 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <p class="ml-2">
+                  Customizing your
+                  <code class="font-bold text-sm text-gray-900">tailwind.config.js</code> file
+                </p>
+              </li>
+              <li class="flex items-start">
+                <span class="flex h-6 items-center sm:h-7">
+                  <svg
+                    class="flex-shrink-0 h-5 text-cyan-500 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <p class="ml-2">
+                  Extracting classes with
+                  <code class="font-bold text-sm text-gray-900">@apply</code>
+                </p>
+              </li>
+              <li class="flex items-start">
+                <span class="flex h-6 items-center sm:h-7">
+                  <svg
+                    class="flex-shrink-0 h-5 text-cyan-500 w-5"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </span>
+                <p class="ml-2">Code completion with instant preview</p>
+              </li>
+            </ul>
+            <p>
+              Perfect for learning how the framework works, prototyping a new idea, or creating a
+              demo to share online.
+            </p>
+          </div>
+          <div class="font-bold text-base pt-10 leading-6 sm:(text-lg leading-7)">
+            <p>Want to dig deeper into Tailwind?</p>
+            <p>
+              <a href="https://tailwindcss.com/docs" class="text-cyan-600 hover:text-red-700">
+                Read the docs &rarr;
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
