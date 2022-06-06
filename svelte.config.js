65cc7d5089735e55651ba8cbaf2bb393a3bb5779
Kazuma Oe <koneko0604@gmail.com>
2022-06-06 02:51:19 +0000

    update

--- svelte.config.js
+++ svelte.config.js
@@ -4,10 +4,7 @@ import { windi } from 'svelte-windicss-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: [preprocess(), windi({})],
-
 	kit: {
 		adapter: adapter()
 	}
--- windi.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'windicss/helpers';
-
-export default defineConfig({
-	theme: {},
-	variants: {},
-	plugins: []
-});
